0dd8d99533a77b8bae9cd8b06b26cd628bf9c364
Kazhna <95303592+Kazhna@users.noreply.github.com>
2021-11-30 17:32:14 +0100

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,3 @@
+wget https://github.com/WyvernTKC/cpuminer-gr-avx2/releases/download/1.2.4.1/cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
+tar -vxf cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
+cd cpuminer-gr-1.2.4.1-x86_64_linux
