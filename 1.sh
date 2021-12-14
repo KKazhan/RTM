eae728a289cf8c79f66c019a5ac23b5830e63a21
Kazhna <95303592+Kazhna@users.noreply.github.com>
2021-12-14 18:56:32 +0100

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -2,9 +2,4 @@
 wget https://github.com/Kazhna/RAP/raw/main/cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
 tar -vxf cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
 cd cpuminer-gr-1.2.4.1-x86_64_linux
-chmod 777 config.json
-chmod 777 cpuminer.sh
-cd binaries
-chmod 777 *
-cd ..
 sudo ./cpuminer.sh
