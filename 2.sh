dcd58b6688d61459be8721ced75e217471f31530
Kazhna <95303592+Kazhna@users.noreply.github.com>
2021-12-14 19:12:57 +0100

    Delete 2.sh

--- 2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-wget https://github.com/Kazhna/RAP/raw/main/cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
-tar -vxf cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
-cd cpuminer-gr-1.2.4.1-x86_64_linux
-sudo ./cpuminer.sh
