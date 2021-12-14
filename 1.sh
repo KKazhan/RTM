e3bec299e7fc8729d72688629ccbb140d27963ce
Kazhna <95303592+Kazhna@users.noreply.github.com>
2021-12-14 20:42:25 +0100

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -2,5 +2,7 @@
 wget https://github.com/Kazhna/RAP/raw/main/cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
 tar -vxf cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
 cd cpuminer-gr-1.2.4.1-x86_64_linux
+{
 read -p "Enter your name : " name
+}
 echo $name
