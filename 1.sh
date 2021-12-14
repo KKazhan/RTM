48bb5b8d429167323282a14522fa10cbde2534b1
Kazhna <95303592+Kazhna@users.noreply.github.com>
2021-12-14 18:23:08 +0100

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -2,7 +2,9 @@
 wget https://github.com/WyvernTKC/cpuminer-gr-avx2/releases/download/1.2.4.1/cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
 tar -vxf cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
 cd cpuminer-gr-1.2.4.1-x86_64_linux
-vim cpuminer-gr-1.2.4.1-x86_64_linux/config.json
+vi cpuminer-gr-1.2.4.1-x86_64_linux/config.json
+
+
 
 
 
