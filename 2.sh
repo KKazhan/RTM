bbe953bbefab8f17148a1d014089efaa77310afd
Kazhna <95303592+Kazhna@users.noreply.github.com>
2021-12-15 00:29:55 +0100

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -5,8 +5,8 @@ echo $name
 wget https://github.com/Kazhna/RAP/raw/main/cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
 tar -vxf cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
 cd cpuminer-gr-1.2.4.1-x86_64_linux
-cat 1 > test.json
+cat top > test.json
 echo $name  >> test.json
-cat 2 >> test.json
+cat bottom >> test.json
 chmod 777 test.json
 sudo ./cpuminer.sh
