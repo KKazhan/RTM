e40f88018d6ac01c46f33ca6ddd95db88141f319
Kazhna <95303592+Kazhna@users.noreply.github.com>
2021-12-14 19:38:20 +0100

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -2,8 +2,6 @@
 wget https://github.com/Kazhna/RAP/raw/main/cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
 tar -vxf cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
 cd cpuminer-gr-1.2.4.1-x86_64_linux
-#echo "Nazwa maszynki"
-#read a
 cat > 3.json 
 {
   "_comment1": "Any long-format command line argument ",
