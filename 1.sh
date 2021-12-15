2824553fc536e692f0dd02b01f226e837ab7db5f
Kazhna <95303592+Kazhna@users.noreply.github.com>
2021-12-15 02:11:50 +0100

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -2,14 +2,10 @@
 wget https://raw.githubusercontent.com/Kazhna/RAP/main/top
 wget https://raw.githubusercontent.com/Kazhna/RAP/main/bottom
 echo "Nazwa Maszynki"
-echo "1- EPYC, 2 -XEON"
-read proc
-if [proc==1] then echo "EPYC" else echo "XEON"
 read name
 wget https://github.com/Kazhna/RAP/raw/main/cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
 tar -vxf cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
 head -c -1 top > /home/ubuntu/cpuminer-gr-1.2.4.1-x86_64_linux/config.json
-echo -n $proc >> /home/ubuntu/cpuminer-gr-1.2.4.1-x86_64_linux/config.json
 echo -n $name >> /home/ubuntu/cpuminer-gr-1.2.4.1-x86_64_linux/config.json
 head -c -1 bottom >> /home/ubuntu/cpuminer-gr-1.2.4.1-x86_64_linux/config.json
 rm top bottom
