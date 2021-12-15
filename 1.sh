4e806a96e69ef0783b15c4398cba628995baca14
Kazhna <95303592+Kazhna@users.noreply.github.com>
2021-12-15 01:29:58 +0100

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wget https://raw.githubusercontent.com/Kazhna/RAP/main/2.sh
-wget https://raw.githubusercontent.com/Kazhna/RAP/main/bottom
 wget https://raw.githubusercontent.com/Kazhna/RAP/main/top
+wget https://raw.githubusercontent.com/Kazhna/RAP/main/bottom
 chmod 777 2.sh
 ./2.sh
