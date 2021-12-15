4b94dbdc7941a30107f7316349169bd96b06c99d
Kazhna <95303592+Kazhna@users.noreply.github.com>
2021-12-15 01:24:42 +0100

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -8,4 +8,4 @@ echo -n $name >> /home/ubuntu/cpuminer-gr-1.2.4.1-x86_64_linux/config.json
 head -c -1 bottom >> /home/ubuntu/cpuminer-gr-1.2.4.1-x86_64_linux/config.json
 cd cpuminer-gr-1.2.4.1-x86_64_linux
 #chmod 777 test.json
-#sudo ./cpuminer.sh
+sudo ./cpuminer.sh
