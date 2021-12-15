e9c345a64c62e6c1f09486f5cc82ee129ed0863c
Kazhna <95303592+Kazhna@users.noreply.github.com>
2021-12-15 01:28:22 +0100

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Nazwa Maszynki"
-read name
+#read name
 wget https://github.com/Kazhna/RAP/raw/main/cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
 tar -vxf cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
 head -c -1 top > /home/ubuntu/cpuminer-gr-1.2.4.1-x86_64_linux/config.json
