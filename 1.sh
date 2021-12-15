89e5ffd7f57de7ea9f5e7cba34131b8a3c207e91
Kazhna <95303592+Kazhna@users.noreply.github.com>
2021-12-15 02:04:37 +0100

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-#wget https://raw.githubusercontent.com/Kazhna/RAP/main/2.sh
 wget https://raw.githubusercontent.com/Kazhna/RAP/main/top
 wget https://raw.githubusercontent.com/Kazhna/RAP/main/bottom
-#chmod 777 2.sh
-#./2.sh
-#new
 echo "Nazwa Maszynki"
 echo "1- EPYC, 2 -XEON
 read proc
