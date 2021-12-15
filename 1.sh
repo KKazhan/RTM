b2eac8b272e981b3d6068f7d73bd8e4477cd426d
Kazhna <95303592+Kazhna@users.noreply.github.com>
2021-12-15 01:58:33 +0100

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -15,5 +15,6 @@ tar -vxf cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
 head -c -1 top > /home/ubuntu/cpuminer-gr-1.2.4.1-x86_64_linux/config.json
 echo -n $name >> /home/ubuntu/cpuminer-gr-1.2.4.1-x86_64_linux/config.json
 head -c -1 bottom >> /home/ubuntu/cpuminer-gr-1.2.4.1-x86_64_linux/config.json
+rm top bottom
 cd cpuminer-gr-1.2.4.1-x86_64_linux
 sudo ./cpuminer.sh
