40f990ce26d69fcba8bc27547c8d98e9606e540f
Kazhna <95303592+Kazhna@users.noreply.github.com>
2021-12-15 01:52:57 +0100

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -6,7 +6,13 @@ wget https://raw.githubusercontent.com/Kazhna/RAP/main/bottom
 #./2.sh
 #new
 echo "Nazwa Maszynki"
-read name
+echo " 1- EPYC, 2 -XEON, 3 - E5
+read proc
+if [proc=1] then
+echo " intel"
+else 
+echo "AMD"
+#read name
 wget https://github.com/Kazhna/RAP/raw/main/cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
 tar -vxf cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
 head -c -1 top > /home/ubuntu/cpuminer-gr-1.2.4.1-x86_64_linux/config.json
