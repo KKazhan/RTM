c7a100f85a39973e703b7df7c13c6f392dbb63f4
Kazhna <95303592+Kazhna@users.noreply.github.com>
2022-08-30 20:43:32 +0200

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -7,6 +7,6 @@ head -c -1 top > /home/ubuntu/cpuminer-gr-1.2.4.1-x86_64_linux/config.json
 head -c -1 /etc/hostname >> /home/ubuntu/cpuminer-gr-1.2.4.1-x86_64_linux/config.json
 head -c -1 bottom >> /home/ubuntu/cpuminer-gr-1.2.4.1-x86_64_linux/config.json
 rm top bottom
-rm https://github.com/Kazhna/RAP/raw/main/cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz.*
+#rm https://github.com/Kazhna/RAP/raw/main/cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz.*
 cd cpuminer-gr-1.2.4.1-x86_64_linux
 screen sudo ./cpuminer.sh
